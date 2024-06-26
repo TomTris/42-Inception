b306a944dffd9a8b760ceb3d29748983684aee66
Quoc Tri Do <qdo@2-H-8.42heilbronn.de>
2024-06-26 19:21:34 +0200

    docker compose

---
+++ srcs/requirements/mariadb/init_mariadb.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+echo "CREATE USER '${WORDPRESS_DB_USER}'@'localhost' IDENTIFIED BY '${WORDPRESS_DB_USER_PASSWORD}';
+CREATE DATABASE ${WORDPRESS_DB_NAME};
+GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* 	TO '${WORDPRESS_DB_USER}'@'${WORDPRESS_DB_HOST}';
+FLUSH PRIVILEGES;" > /run/mysql/init_db.sql
+
+mariadb-install-db
+
+exec mariadb --user=mysql
