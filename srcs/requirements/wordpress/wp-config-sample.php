1c329292daac324f916abd03a438afef59a47d37
Quoc Tri Do <qdo@1-C-22.42heilbronn.de>
2024-06-28 17:14:51 +0200

    wordpress added config

--- srcs/requirements/wordpress/wp-config-sample.php
+++ srcs/requirements/wordpress/wp-config-sample.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'getenv(MYSQL_DATABASE)' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'getenv(WORDPRESS_USER1)' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'getenv(WORDPRESS_USER1_PASSWORD)' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'getenv(WORDPRESS_DB_HOST)' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         '77-)Wzov$~8h9ax&j;gc?bq/jhMn:S$8kbQ+]mEj<v3lzyOjj-.e,gU-%,+uhUxj');
+define('SECURE_AUTH_KEY',  '(OSuGkOd(3HEsppr_+h(4iTV54~w+pz0Sc=2^5~=(U.dC9YNs)Fa|F~@piK8,5]k');
+define('LOGGED_IN_KEY',    'vK09oD120cJ`koD)yketB4gczJWf4-*5l/7K8J5+,Vr6DW.@^V4DDi,)p[#(fVsk');
+define('NONCE_KEY',        ']}6$f-7g0YmOkL)QB:d%#vR:DhuO;?EPj~YSxUpCjfOyjsZ2}#pl+#,5^$ZrNj3T');
+define('AUTH_SALT',        'kdICh8oNq~+8]Fs{fHl$::^&[--(0zh-/!Y9F[`!9VWbnL/-CX(Rr6HI9xWu_X8V');
+define('SECURE_AUTH_SALT', 'w$7d!Tk30aUC8>~NCLM4?Y.ctB_VHK3>9L8iO$<Dq.dq+#&#4 *stWWInwUA6nqY');
+define('LOGGED_IN_SALT',   '1t]12UCV[){q_U|s~btxP!EU<V]uGG=n;)jv_O+-@|qD+^@k0$3P:JZyOL.NLf`F');
+define('NONCE_SALT',       ',S*x-hruL`USbv|#C&G!Uaw9Avn@SO-jh`A?Clp!emWjL3?Vodon(H xq]]]ZD~I');
 
 /**#@-*/
 
@@ -89,7 +89,8 @@ define( 'WP_DEBUG', false );
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-        define( 'ABSPATH', __DIR__ . '/' );
+        define( 'ABSPATH', '/var/www/html/' );
 }
 
 /** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
