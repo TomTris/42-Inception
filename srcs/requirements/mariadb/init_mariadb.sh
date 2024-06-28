2aaef8206629b3ced77d1cbf89eb11c08982d979
TomTris <quoctrido.jb@gmail.com>
2024-06-28 18:18:49 +0200

    added again echo, mariadb container works well

--- srcs/requirements/mariadb/init_mariadb.sh
+++ srcs/requirements/mariadb/init_mariadb.sh
@@ -2,7 +2,7 @@
 service mariadb start
 sleep 5
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > script.sql
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 echo "CREATE USER IF NOT EXISTS'$WORDPRESS_USER1'@'$WORDPRESS_DB_HOST' IDENTIFIED BY '$WORDPRESS_USER1_PASSWORD';" >> script.sql;
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$WORDPRESS_USER1'@'$WORDPRESS_DB_HOST';" >> script.sql;
 echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> setup.sql
@@ -24,7 +24,5 @@ LIMITER
 
 service mariadb stop
 sleep 5
-
-
 
 exec mysqld
