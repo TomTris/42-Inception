5deaf681072254e6d771a679510daed0efb6e316
Quoc Tri Do <qdo@1-C-22.42heilbronn.de>
2024-06-28 15:24:46 +0200

    added secure installation

--- srcs/requirements/mariadb/init_mariadb.sh
+++ srcs/requirements/mariadb/init_mariadb.sh
@@ -2,17 +2,29 @@
 service mariadb start
 sleep 5
 
-echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" > script.sql
-echo "CREATE USER IF NOT EXISTS'${WORDPRESS_USER1}'@'${WORDPRESS_DB_HOST}' IDENTIFIED BY '${WORDPRESS_USER1_PASSWORD}';" >> script.sql;
-echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${WORDPRESS_USER1}'@'${WORDPRESS_DB_HOST}';" >> script.sql;
-echo "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;" >> setup.sql
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > script.sql
+echo "CREATE USER IF NOT EXISTS'$WORDPRESS_USER1'@'$WORDPRESS_DB_HOST' IDENTIFIED BY '$WORDPRESS_USER1_PASSWORD';" >> script.sql;
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$WORDPRESS_USER1'@'$WORDPRESS_DB_HOST';" >> script.sql;
+echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> setup.sql
 echo "FLUSH PRIVILEGES;" >> script.sql;
 mysql < script.sql
 
 mysql -u root < script.sql
 
-service mariadb stop
 
+mysql_secure_installation <<LIMITER
+$MYSQL_ROOT_PASSWORD
+n
+n
+y
+n
+y
+y
+LIMITER
+
+service mariadb stop
 sleep 5
+
+
 
 exec mysqld
