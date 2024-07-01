ab977520e2fd421183bff2a3e3677b91d4067215
TomTris <quoctrido.jb@gmail.com>
2024-07-01 10:59:04 +0200

    Mysql_host to Wordpress db host

--- srcs/requirements/mariadb/init_mariadb.sh
+++ srcs/requirements/mariadb/init_mariadb.sh
@@ -6,7 +6,7 @@ sleep 5
 echo "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` ;" > script.sql
 echo "CREATE USER IF NOT EXISTS '${WORDPRESS_USER1}'@'${WORDPRESS_DB_HOST}' IDENTIFIED BY '${WORDPRESS_USER1_PASSWORD}';" >> script.sql
 echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${WORDPRESS_USER1}'@'${WORDPRESS_DB_HOST}';" >> script.sql
-echo "ALTER USER 'root'@'${MYSQL_DB_HOST}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >> script.sql
+echo "ALTER USER 'root'@'${WORDPRESS_DB_HOST}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >> script.sql
 echo "FLUSH PRIVILEGES;" >> script.sql
 
 mysql -u root -p < script.sql
